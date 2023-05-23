67c19599986523c10b7f77c1f61e21aa6239bbfe
Yoshioka <sss.yoshioka@gmail.com>
2023-05-23 16:31:03 +0000

    🎸 feat: add .iex.exs

---
+++ .iex.exs
@@ -0,0 +1,11 @@
+IO.puts("====-====-====-====-====")
+IO.puts(".iex.exs")
+
+IO.puts("* Alias Modules")
+alias ExlChain.LLM
+alias ExlChain.LLM.OpenAI
+alias ExlChain.Template
+alias ExlChain.Chain
+
+IO.puts("====-====-====-====-====")
+
