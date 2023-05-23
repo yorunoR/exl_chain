8ac2ef22fddf4029e34ace4cf2aeb8f022f92806
Yoshioka <sss.yoshioka@gmail.com>
2023-05-23 17:58:04 +0000

    🎸 feat: add temperature to OpenAI LLM

--- lib/exl_chain/llm/open_ai.ex
+++ lib/exl_chain/llm/open_ai.ex
@@ -1,21 +1,39 @@
 defmodule ExlChain.LLM.OpenAI do
-  defstruct client: nil, model: nil
+  defstruct client: nil, model: nil, temperature: nil
 
   alias __MODULE__
   alias Eoai.Client
   alias Eoai.Request
   alias Eoai.Response
 
-  def new(model \\ nil) do
+  def new() do
+    %OpenAI{
+      client: Client.new()
+    }
+  end
+
+  def new(model) when is_binary(model) do
     %OpenAI{
       client: Client.new(),
       model: model
     }
   end
 
+  def new(opts) when is_list(opts) do
+    model = Keyword.get(opts, :model)
+    temperature = Keyword.get(opts, :temperature)
+
+    %OpenAI{
+      client: Client.new(),
+      model: model,
+      temperature: temperature
+    }
+  end
+
   def chat(llm, content) do
     params = %{
       model: llm.model || "gpt-3.5-turbo",
+      temperature: llm.temperature || 1,
       messages: [
         %{role: "user", content: content}
       ]
